1101d2637a3279a5929d524c300b782838be6f79
Prashant Agrawal <a.prashant2021@gmail.com>
2024-01-04 18:53:04 +0530

    adding mongo config

--- src/middlewares/mongo/MongoAccess.ts
+++ src/middlewares/mongo/MongoAccess.ts
@@ -1,15 +1,27 @@
-import * as express from "express"
 import mongoose from "mongoose"
 import kernel from "../../ioc/kernel"
 import { TYPES } from "../../ioc/types"
 import { ILogger } from "../../logger/ILogger"
-const logger = kernel.get<ILogger>(TYPES.ILogger)
-const DATABASE_NAME: string = "speer-assignment"
-const MONGO_URL: string = `mongodb://localhost:27017/${DATABASE_NAME}`
-// This is mongodb local connection
+import { Configuration } from "../../conf/Configuration"
+import * as _ from "lodash"
+const logger: ILogger = kernel.get<ILogger>(TYPES.ILogger)
+const appConfig = kernel
+  .get<Configuration>(TYPES.Configuration)
+  .getConfiguration()
+const databaseName: string = _.get(
+  appConfig,
+  "mongoConfig.database",
+  "speer-assignment",
+)
+const mongoUrl: string = _.get(appConfig, "mongoConfig.mongoUrl", "") + "/"
+const user: string = _.get(appConfig, "mongoConfig.user", "")
+const password: string = _.get(appConfig, "mongoConfig.password", "")
+const connectionString = mongoUrl + databaseName
 export const connectMongo = (): Promise<typeof mongoose> => {
   logger.info("Trying to connect to mongo...")
-  return mongoose.connect(MONGO_URL, {
+  return mongoose.connect(connectionString, {
+    user: user,
+    pass: password,
     retryWrites: true,
     w: "majority",
   })
